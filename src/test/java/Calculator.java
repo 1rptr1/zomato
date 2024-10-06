1d784f831ffc8ef1cba40d9602de6ba3dda2ad13
saura <sauravbhr12@gmail.com>
2024-10-06 12:15:01 +0530

    Updated Vulnerability issue and fixed folder structure

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -6,13 +6,12 @@ import java.net.URL;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
 
-public class cdds
+public class Calculator
 {
 WebDriver driver;
 
